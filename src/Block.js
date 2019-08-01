5342077ce10770f5323b35eee56f68580d33bd65
Daniel Mason <daniel@danielmason.com>
2019-08-01 20:51:40 +0100

    feat(drop): implemented empty and isEmpty

--- src/Block.js
+++ src/Block.js
@@ -1,10 +1,19 @@
 export const COLOURS = ['red', 'green', 'blue', 'yellow'];
+export const empty = 'grey';
 
 class Block {
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+  }
+
+  empty() {
+    this.colour = empty;
+  }
+
+  isEmpty() {
+    return this.colour === empty;
   }
 }
 
